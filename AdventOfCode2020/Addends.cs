ed4c3a90acf817ba20e2f327a3085038e895a74a
Johan Wintgens <johan.wintgens@hotmail.com>
2020-12-01 17:09:37 +0100

    Add project files.

---
+++ AdventOfCode2020/Addends.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode2020
+{
+    public static class Addends
+    {
+        public static int FindAndMultiply(IEnumerable<int> possibleAddends, int sum)
+        {
+            foreach (var x in possibleAddends)
+            foreach (var y in possibleAddends)
+            foreach (var z in possibleAddends)
+                if (sum == x + y + z)
+                    return x * y * z;
+
+            return 0;
+        }
+    }
+}
