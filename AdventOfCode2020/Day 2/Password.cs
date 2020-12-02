158dc38829b7638293d2c5b197bb6f28f3878cfc
Johan Wintgens <johan.wintgens@hotmail.com>
2020-12-02 17:26:38 +0100

    [+] Completed Day 2.1

---
+++ AdventOfCode2020/Day 2/Password.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode2020
+{
+    public class Password
+    {
+        public string Value { get; }
+        public char KeyChar { get; }
+        public uint MinOccurrence { get; }
+        public uint MaxOccurrence { get; }
+
+        public Password(string value, char keyChar, uint minOccurrence, uint maxOccurrence)
+        {
+            Value = value;
+            KeyChar = keyChar;
+            MinOccurrence = minOccurrence;
+            MaxOccurrence = maxOccurrence;
+        }
+
+        public bool IsValid()
+        {
+            var occurrence = Value.Count(x => x == KeyChar);
+
+            return occurrence >= MinOccurrence && occurrence <= MaxOccurrence;
+        }
+
+        public static Password FromLine(string line)
+        {
+            var spaceSeparated = line.Split(' ');
+
+            var limits = spaceSeparated[0].Split('-');
+            var minOccurrence = Convert.ToUInt32(limits[0]);
+            var maxOccurrence = Convert.ToUInt32(limits[1]);
+
+            var keyChar = spaceSeparated[1][0];
+
+            var value = spaceSeparated[2];
+
+            return new Password(value, keyChar, minOccurrence, maxOccurrence);
+        }
+    }
+}
